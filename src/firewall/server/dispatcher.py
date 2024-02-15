37aaa1d2cae117befc4ff26a7dd4d8fc252c5f46
Donald Yandt <10255876+TorontoMedia@users.noreply.github.com>
2024-02-15 12:54:05 -0500

    feat(dispatcher): use GLib spawn async

--- src/firewall/server/dispatcher.py
+++ src/firewall/server/dispatcher.py
@@ -1,10 +1,24 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+from gi.repository import GLib
+
 import os
-import subprocess
 
 from firewall.core.logger import log
 from firewall.config import FIREWALLD_DISPATCHER, ETC_FIREWALLD_DISPATCHER, COMMANDS
+
+
+def handle_completion(pid, status, data):
+    log.debug1(
+        f"dispatcher completion handler for file: '{data[0]} interface: '{data[1]}' signal: {data[2]}."
+    )
+    try:
+        GLib.spawn_check_exit_status(status)
+    except GLib.GError:
+        log.debug1(
+            f"error running dispatcher for file: '{data[0]} interface: '{data[1]}' signal: {data[2]}."
+        )
+    GLib.spawn_close_pid(pid)
 
 
 def run_dispatcher(interface, signal, args):
@@ -17,20 +31,19 @@ def run_dispatcher(interface, signal, args):
                 if os.path.isfile(_path) and os.access(_path, os.X_OK):
                     yield _path
 
-    env = {"LANG": "C"}
-    prog_args1 = [COMMANDS["systemd-run"], "--no-block", "--property", "TimeoutSec=60"]
-    prog_args2 = [interface, signal] + [str(a) for a in args]
-
+    environment = ["LANG=C", "LANGUAGE=C", "LC_ALL=C", "LC_MESSAGES=C"]
+    executor = [COMMANDS["systemd-run"], "--no-block", "--property", "TimeoutSec=60"]
+    arguments = [interface, signal] + [str(a) for a in args]
     for file in _dispatchers([FIREWALLD_DISPATCHER, ETC_FIREWALLD_DISPATCHER]):
-        try:
-            subprocess.Popen(
-                prog_args1 + [file] + prog_args2,
-                stdin=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                stdout=subprocess.DEVNULL,
-                close_fds=True,
-                env=env,
-            )
-        except OSError:
-            log.debug1(f"Failed to call dispatcher: {file}.")
-            pass
+        process_arguments = [file] + arguments
+        process = GLib.spawn_async(
+            executor + process_arguments,
+            envp=environment,
+            flags=GLib.SPAWN_DO_NOT_REAP_CHILD,
+            standard_output=False,
+            standard_error=False,
+        )
+
+        GLib.child_watch_add(
+            GLib.PRIORITY_DEFAULT, process[0], handle_completion, process_arguments
+        )
